314edda60106f186b461c205915d98a826ca1a27
mackilanu <mackilanu@gmail.com>
2020-06-07 21:01:53 +0200

    first commit

---
+++ Game.hpp
@@ -0,0 +1,35 @@
+//
+// Created by marcus on 2020-06-06.
+//
+
+#ifndef TEST_SDL_GAME_HPP
+#define TEST_SDL_GAME_HPP
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <iostream>
+
+class Game {
+public:
+    Game();
+    ~Game();
+
+    void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+    void handleEvents();
+    void update();
+    void render();
+    void clean();
+
+    bool running() {return isRunning;};
+
+private:
+    int cnt = 0;
+    bool isRunning;
+    SDL_Window *window;
+    SDL_Renderer  *renderer;
+};
+
+
+#endif //TEST_SDL_GAME_HPP
---
+++ cmake-build-debug/CMakeFiles/mcengine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mcengine.dir/Game.cpp.o"
+  "CMakeFiles/mcengine.dir/main.cpp.o"
+  "mcengine"
+  "mcengine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mcengine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
